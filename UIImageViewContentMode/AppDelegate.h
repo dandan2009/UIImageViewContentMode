69b7214572c63535d470825fb7806a08d86a6650
sym <sym@qq.com>
2018-11-10 16:37:26 +0800

    Initial Commit

---
+++ UIImageViewContentMode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImageViewContentMode
+//
+//  Created by shan on 2018/11/10.
+//  Copyright © 2018年 shan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIImageViewContentMode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImageViewContentMode
+//
+//  Created by shan on 2018/11/10.
+//  Copyright © 2018年 shan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
